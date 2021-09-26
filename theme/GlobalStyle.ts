fae1b6f49bee8030aa8dbcb062fce0bd443958d6
leo <algo2000@naver.com>
2021-09-27 04:18:51 +0900

    fix 잘못 기입된 폰트 수정

--- theme/GlobalStyle.ts
+++ theme/GlobalStyle.ts
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.backgroundColor.bg};
     color: ${({ theme }) => theme.fontColor.main};
-    font-family: "Noto Sans KR", sans-serif !important;
+    font-family: "Noto Sans CJK KR", sans-serif !important;
     transition: all 0.25s linear;
     margin: 0;
     padding: 0;
